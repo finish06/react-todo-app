0298b32a0463fb0d5facad700460ac393b600ad1
Caleb Dunn <finish.06@gmail.com>
2020-06-27 17:02:26 -0500

    Updates

---
+++ precache-manifest.f383f11e6621da126289748019483675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cac26d2708f5dcf343e9843ec5b83a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "ee28f7f1b911dc34bfef",
+    "url": "/react-todo-app/static/css/main.d133d587.chunk.css"
+  },
+  {
+    "revision": "98f04a78a60c7b713ab5",
+    "url": "/react-todo-app/static/js/2.ede55c41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.ede55c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee28f7f1b911dc34bfef",
+    "url": "/react-todo-app/static/js/main.cf774f69.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
